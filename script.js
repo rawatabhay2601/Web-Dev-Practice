9be29dd8d8badbf8b88fdff6fec4bef92be2c670
rawatabhay2601 <rawatabhay2601@gmail.com>
2023-02-07 19:29:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var header = document.getElementById('main-title');
+header.style.borderBottom = "solid 2px #000";
+
+var items = document.getElementById('items');
+items.style.fontWeight = "solid";
+items.style.color = "green";
