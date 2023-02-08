64b4784363dad62fd560ffba406245db53b2dc64
rawatabhay2601 <rawatabhay2601@gmail.com>
2023-02-08 15:07:05 +0530

    Task6Commit

--- script.js
+++ script.js
@@ -12,9 +12,28 @@ for(let i of item){
     i.style.fontWeight = "bold";
 }
 
+// task 6
 var newItems = document.getElementsByTagName('li');
 
 for(let i of newItems){
     i.style.fontStyle = "italic";
     i.style.fontWeight = "bold";
 }
+
+// task 7
+// adding green background to second element
+item[1].style.backgroundColor = 'lightgreen';
+
+// making the third item invisible
+item[2].innerText = "";
+item[2].style.padding = "3.7%"
+
+// using QuerySelectorAll
+var query = document.querySelectorAll('li');
+query[1].style.color = "";
+
+for(i=0; i<query.length;i += 2){
+    query[i].style.backgroundColor = "green";
+}
+
+console.log(query.length);
